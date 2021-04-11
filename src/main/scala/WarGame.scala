261f709fe3898220fa1d8d0139d0d3969083ae35
Dominykas Antanas Diržys <dominykasdirzys@gmail.com>
2021-04-11 14:46:32 +0300

    next function

--- src/main/scala/WarGame.scala
+++ src/main/scala/WarGame.scala
@@ -89,26 +89,55 @@ object WarGame extends App {
 
   val initState = initialState(hand1, hand2)
 
-  val rand = scala.util.Random
+  val rand       = scala.util.Random
   val trumpSuite = Suite.values(rand.nextInt(4))
+  println("Trump suite is - " + trumpSuite)
 
   def next(state: State): State = {
-    val player1Hand = state.player1.hand.toList
-    val player2Hand = state.player2.hand.toList
+    var player1Hand = state.player1.hand
+    var player2Hand = state.player2.hand
     var player1ScorePile = state.player1.scorePile
-    val player2ScorePile = state.player2.scorePile
+    var player2ScorePile = state.player2.scorePile
 
-    for (x <- 0 to player1Hand.size - 1) {
-      val player1Card = player1Hand(x)
-      val player2Card = player2Hand(x)
+    val player1Card       = player1Hand.head
+    val player2Card       = player2Hand.head
+    val p1CardIsTrumpCard = player1Card.suite == trumpSuite
+    val p2CardIsTrumpCard = player2Card.suite == trumpSuite
 
-      if (player1Card.suite == trumpSuite && player2Card.suite == trumpSuite) {
-        if (player1Card.rank.value > player2Card.rank.value) {
-          player1ScorePile += player1Card
-          player1ScorePile += player2Card
-        }
+    def fight(): Unit = {
+      if (player1Card.rank.value > player2Card.rank.value) {
+        println(player1Card + " beats " + player2Card)
+        player1ScorePile += player1Card
+        player1ScorePile += player2Card
+      } else if (player1Card.rank.value == player2Card.rank.value) {
+        println(player1Card + " ties with " + player2Card)
+        player1ScorePile += player1Card
+        player2ScorePile += player2Card
+      } else {
+        println(player2Card + " beats " + player1Card)
+        player2ScorePile += player1Card
+        player2ScorePile += player2Card
       }
     }
+
+    if (p1CardIsTrumpCard && p2CardIsTrumpCard) {
+      fight()
+    } else if (p1CardIsTrumpCard && !p2CardIsTrumpCard) {
+      println(player1Card + " beats " + player2Card)
+      player1ScorePile += player1Card
+      player1ScorePile += player2Card
+    } else if (!p1CardIsTrumpCard && p2CardIsTrumpCard) {
+      println(player2Card + " beats " + player1Card)
+      player2ScorePile += player1Card
+      player2ScorePile += player2Card
+    } else {
+      fight()
+    }
+
+    player1Hand -= player1Card
+    player2Hand -= player2Card
+
+    State(Player(player1Hand, player1ScorePile), Player(player2Hand, player2ScorePile))
   }
 
   def play(initialState: State): GameResult = ???
